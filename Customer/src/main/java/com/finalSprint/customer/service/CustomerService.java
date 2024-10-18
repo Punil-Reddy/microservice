9cf0c461d88c31fd027594f2cb85130f6112e54b
Punil-Reddy <139474412+Punil-Reddy@users.noreply.github.com>
2024-10-18 16:03:22 +0530

    Add files via upload

---
+++ Customer/src/main/java/com/finalSprint/customer/service/CustomerService.java
@@ -0,0 +1,52 @@
+package com.finalSprint.customer.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finalSprint.customer.exceptions.InvalidValueException;
+import com.finalSprint.customer.feignClient.CustomerFeign;
+import com.finalSprint.customer.model.Customer;
+import com.finalSprint.customer.model.Product;
+import com.finalSprint.customer.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+	
+	@Autowired
+	private CustomerRepository repository;
+	
+	@Autowired
+	private CustomerFeign client;
+	
+	public Customer saveCustomer(Customer c) {
+		
+		
+		return repository.save(c);
+	}
+	
+	public List<Customer> findAllCustomers(){
+		return repository.findAll();
+	}
+	
+	public Customer findCustomerById(Long id) throws InvalidValueException, Exception {
+		return repository.findById(id).orElseThrow(()-> new InvalidValueException("Customer not found - Invalid id"));
+	}
+	
+	public void deleteCustomer(Long id) throws InvalidValueException, Exception{
+		if(!repository.existsById(id)) {
+			throw new InvalidValueException("Customer not found- Invalid id");
+		}
+		repository.deleteById(id);
+	}
+	
+//	---------------------------------------------------------
+//	Methods for feign
+	
+	public List<Product> findAllProducts(){
+		return client.showProducts();
+	}
+	
+	
+}
